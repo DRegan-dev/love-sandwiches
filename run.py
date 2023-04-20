668fab11169b39eb754a83670065bcb8d01540fc
DRegan-dev <damienreganmusic@gmail.com>
2023-04-20 22:44:02 +0000

    final code

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -26,7 +25,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: \ns")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
 
@@ -133,4 +132,4 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-# main()
+main()
